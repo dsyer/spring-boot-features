91155e053c809a3afafd4332ae685cd8ce954ee5
Dave Syer <dsyer@vmware.com>
2021-05-26 13:43:57 +0100

    Try it out with native images

--- samples/application/src/main/java/app/main/SampleApplication.java
+++ samples/application/src/main/java/app/main/SampleApplication.java
@@ -3,7 +3,10 @@ package app.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.config.BasicConfigurations;
 import org.springframework.boot.config.SpringBootFeaturesApplication;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.hint.TypeHints;
 
+@TypeHints(@TypeHint(types = BasicConfigurations.class))
 @SpringBootFeaturesApplication(BasicConfigurations.class)
 public class SampleApplication {
 
--- samples/jackson/src/main/java/app/main/SampleApplication.java
+++ samples/jackson/src/main/java/app/main/SampleApplication.java
@@ -1,18 +1,22 @@
 package app.main;
 
+import reactor.core.publisher.Mono;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.config.JacksonConfigurations;
 import org.springframework.boot.config.SpringBootFeaturesApplication;
 import org.springframework.boot.config.WebFluxConfigurations;
 import org.springframework.context.annotation.Bean;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.nativex.hint.TypeHints;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
-import reactor.core.publisher.Mono;
-
+@TypeHints(@TypeHint(types = { JacksonConfigurations.class,
+		WebFluxConfigurations.class }))
 @SpringBootFeaturesApplication({ JacksonConfigurations.class,
 		WebFluxConfigurations.class })
 public class SampleApplication {
